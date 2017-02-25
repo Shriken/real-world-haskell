9505c29eb7b2cbb13e13d3e36fd0e08974847ed4
Zane Sterling <sterling.zane@gmail.com>
2017-02-25 00:11:54 -0500

    Add more ch. 4 excercises

--- ch4-excercises.hs
+++ ch4-excercises.hs
@@ -1,4 +1,7 @@
 -- file: ch4-excercises.hs
+import Data.Char
+import Data.List
+
 
 splitWith :: (a -> Bool) -> [a] -> [[a]]
 splitWith _    []     = []
@@ -7,5 +10,33 @@ splitWith pred (x:xs) | pred x    = splitWith pred xs
     where (first_hunk, rest) = break pred $ x:xs
 
 
-asInt_fold :: String -> Int
-asInt_fold s :: foldl s
+type ErrorMessage = String
+asInt_fold :: String -> Either ErrorMessage Int
+asInt_fold []       = Left "expected an int, found no digits"
+asInt_fold ('-':xs) = case asInt_fold xs of
+    Left  msg -> Left msg
+    Right x   -> Right $ negate x
+asInt_fold s        = foldl' addDigit (Right 0) s
+    where addDigit (Left msg)  x                         = Left msg
+          addDigit (Right acc) x | not $ isDigit x       = Left "non-digit character found"
+                                 | div maxBound 10 < acc = Left "number too large"
+                                 | otherwise             = Right $ acc * 10 + (digitToInt x)
+
+
+myConcat :: [[a]] -> [a]
+myConcat l = foldr (++) [] l
+
+
+takeWhileRecursive :: (a -> Bool) -> [a] -> [a]
+takeWhileRecursive p (x:xs) = if p x then x : takeWhileRecursive p xs else []
+
+takeWhileFold :: (a -> Bool) -> [a] -> [a]
+takeWhileFold p l = foldr step [] l
+    where step x acc = if not $ p x then [] else x:acc
+
+
+groupBy :: (a -> a -> Bool) -> [a] -> [[a]]
+groupBy _ [] = []
+groupBy c l  = reverse $ foldl step [[head l]] $ tail l
+    where step ((a:as):ass) x | a `c` x   = (a:as ++ [x]):ass
+                              | otherwise = [x]:(a:as):ass
